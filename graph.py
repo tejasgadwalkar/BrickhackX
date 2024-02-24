b2ce7571bd90e7f6ba01e01146623457642ce6da
tsg1472 <tsg1472@rit.edu>
2024-02-24 18:43:02 -0500

    Added functionality for graphs

--- graph.py
+++ graph.py
@@ -1,32 +1,40 @@
-import networkx as nx
-import address as addr
-
-def build_weighted_graph(graph: nx.Graph):
-    # nodes = graph.nodes()
-    # for name in [nodes[node]['name'] for node in nodes]:
-    #     for neighbor in graph.neighbors(graph, name):
-    #         weight = addr.get_driving_time(name, neighbor['name'])
-    #        graph.add_edge(name, neighbor, weight = weight)
-
-    nodes = graph.nodes()
-    for node in nodes:
-        neighbors = node
-
-    return graph
+import nodes
 
 
+class Graph:
 
-def build_nodes(graph, addresses):
-    for address in addresses:
-        graph.add_nodes(address)
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
 
+    def add_node(self, node):
+        self.nodes.append(node)
 
+    def add_edge(self, node1, node2, weight):
+        self.edges.append([node1, node2, weight])
 
-def build_graph(addresses):
-    graph = nx.Graph()
-    k = 0
-    for i in range(n):
-        for j in range(i, n):
-            graph.add_edge(i, j, weight=weights[k])
-            k += 1
-    return graph
+    def get_node(self, name):
+        tmp = nodes.Node(name)
+        for node in self.nodes:
+            if node == tmp:
+                return node
+
+    def update_weight(self, name1, name2, weight):
+        node1 = nodes.Node(name1)
+        node2 = nodes.Node(name2)
+        for edge in self.edges:
+            if edge[0] == node1 and edge[1] == node2:
+                edge[2] = weight
+
+    def get_weight(self, name1, name2):
+        node1 = nodes.Node(name1)
+        node2 = nodes.Node(name2)
+        for edge in self.edges:
+            if edge[0] == node1 and edge[1] == node2:
+                return edge[2]
+
+    def shortest_path(self, start, end):
+        node_s = nodes.Node(start)
+        node_e = nodes.Node(end)
+        # TODO
+        return None
---
+++ nodes.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, name):
+        self.neighbors = []
+        self.name = name
+
+    def get_neighbors(self):
+        return self.neighbors
+
+    def add_neighbor(self, node):
+        self.neighbors.append(node)
+
+    def __eq__(self, other):
+        return self.name == other.name
