8c8797697bbd051e768964d2be40840a6bbd5aed
Madeline Mariano <mam5090@rit.edu>
2024-02-25 02:02:24 -0500

    added pathway to show on map

--- main.py
+++ main.py
@@ -1,42 +1,3 @@
-# import networkx as nx
-# import requests as rq
-# import graph
-# import address as ad
-# from flask import Flask, render_template, request
-# import json
-
-# app = Flask(__name__)
-# addresses = []
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         address_temp = request.form['address']
-#         address = ad.validate_address(address_temp)
-#         addresses.append(address)
-#         return render_template('index.html', addresses=addresses)
-#     return render_template('index.html', addresses=addresses)
-
-
-# @app.route('/locations', methods=['POST'])
-# def locations():
-#     loc1 = request.form['name']
-#     loc2 = request.form['email']
-#     return loc1 + loc2
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# from flask import Flask, render_template, request
-
-# app = Flask(__name__)
-# addresses = []
-
-# while(1):
-#     print(addresses)
-
 import networkx as nx
 import requests as rq
 import graph
@@ -65,6 +26,7 @@ def add_address():
     addresses.append(address)
     return jsonify({'message': 'Address added successfully'})
 
+
 @app.route('/get_addresses', methods=['GET'])
 def get_addresses():
     return jsonify({'addresses': addresses})
@@ -85,6 +47,7 @@ def delete_address():
 def build_path():
     mapGraph = bg.build_graph(addresses)
     return jsonify({'message': str(mapGraph)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
