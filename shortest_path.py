3c6885eb666ec61aff2cf4ac280a5125e839861e
Madeline Mariano <mam5090@rit.edu>
2024-02-24 23:10:54 -0500

    changed init path algo

--- shortest_path.py
+++ shortest_path.py
@@ -1,10 +1,14 @@
-def path_helper(g, start, n_rest):
+import graph
+
+
+def path_helper(g: graph.Graph, start, n_rest):
     """
-    :param g: graph
+    :param g: graph of Graph class
     :param start: starting node's name
     :param n_rest: list of nodes not in path already
     :return: the name of the node with the smallest weight
     """
+
     shortest = n_rest[0].name
     for node in n_rest:
         if g.get_weight(start, node.name) < g.get_weight(start, shortest):
@@ -12,9 +16,9 @@ def path_helper(g, start, n_rest):
     return shortest
 
 
-def init_path(g, start):
+def init_path(g: graph.Graph, start):
     """
-    :param g: graph
+    :param g: graph of Graph class
     :param start: name of starting node
     :return: path of names by always choosing the shortest weight
     """
@@ -22,7 +26,7 @@ def init_path(g, start):
     n_rest.remove(start)
     path = [start]
     while n_rest:
-        next_node = g.path_helper(start, n_rest)
+        next_node = path_helper(g, start, n_rest)
         path += next_node
         start = next_node
         n_rest.remove(start)
