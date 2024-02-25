02a6fa8529dd4edf638a5fefc40f0907ae170ec2
Madeline Mariano <mam5090@rit.edu>
2024-02-24 23:03:48 -0500

    started init path algo

---
+++ shortest_path.py
@@ -0,0 +1,29 @@
+def path_helper(g, start, n_rest):
+    """
+    :param g: graph
+    :param start: starting node's name
+    :param n_rest: list of nodes not in path already
+    :return: the name of the node with the smallest weight
+    """
+    shortest = n_rest[0].name
+    for node in n_rest:
+        if g.get_weight(start, node.name) < g.get_weight(start, shortest):
+            shortest = node.name
+    return shortest
+
+
+def init_path(g, start):
+    """
+    :param g: graph
+    :param start: name of starting node
+    :return: path of names by always choosing the shortest weight
+    """
+    n_rest = g.nodes
+    n_rest.remove(start)
+    path = [start]
+    while n_rest:
+        next_node = g.path_helper(start, n_rest)
+        path += next_node
+        start = next_node
+        n_rest.remove(start)
+    return path
