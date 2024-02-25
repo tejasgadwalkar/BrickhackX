e7c7800c3f8a8f5085f287aeaab44cbdf490e067
Maanav Contractor (mpc9618) <mpc9618@rit.edu>
2024-02-25 00:42:55 -0500

    adjusting small things, doing stuff and making progress

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
+# import networkx as nx
+# import requests as rq
+# import graph
+# import address as ad
+# from flask import Flask, render_template, request
+# import json
+
+# app = Flask(__name__)
+# addresses = []
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         address_temp = request.form['address']
+#         address = ad.validate_address(address_temp)
+#         addresses.append(address)
+#         return render_template('index.html', addresses=addresses)
+#     return render_template('index.html', addresses=addresses)
+
+
+# @app.route('/locations', methods=['POST'])
+# def locations():
+#     loc1 = request.form['name']
+#     loc2 = request.form['email']
+#     return loc1 + loc2
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+# addresses = []
+
+# while(1):
+#     print(addresses)
+
 import networkx as nx
 import requests as rq
 import graph
 import address as ad
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 addresses = []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,19 +57,28 @@ def index():
     return render_template('index.html', addresses=addresses)
 
 
-@app.route('/locations', methods=['POST'])
-def locations():
-    loc1 = request.form['name']
-    loc2 = request.form['email']
-    return loc1 + loc2
+@app.route('/add_address', methods=['POST'])
+def add_address():
+    data = request.json
+    address = data['address']
+    addresses.append(address)
+    return jsonify({'message': 'Address added successfully'})
+
+@app.route('/get_addresses', methods=['GET'])
+def get_addresses():
+    return jsonify({'addresses': addresses})
+
+
+@app.route('/delete_address', methods=['POST'])
+def delete_address():
+    data = request.json
+    address = data['address']
+    if address in addresses:
+        addresses.remove(address)
+        return jsonify({'message': 'Address deleted successfully'})
+    else:
+        return jsonify({'message': 'Address not found'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-addresses = []
-
