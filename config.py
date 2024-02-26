edfbf6f3fee8720fe26aa05f4cb3008d341ab11e
tsg1472 <tsg1472@rit.edu>
2024-02-26 00:47:32 -0500

    Added files for deployment

---
+++ config.py
@@ -0,0 +1,17 @@
+# https://developers.redhat.com/articles/2023/08/17/how-deploy-flask-application-python-gunicorn#containerization
+import os
+
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+
+
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
